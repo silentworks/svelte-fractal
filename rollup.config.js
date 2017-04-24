a629230476d76136cfe3e6dfb57c5030bcf17c91
Andrew Smith <a.smith@silentworks.co.uk>
2017-04-24 13:20:10 +0000

    Update with actual rendering happening now

---
+++ rollup.config.js
@@ -0,0 +1,71 @@
+import * as fs from 'fs';
+import serve from 'rollup-plugin-serve';
+import svelte from 'rollup-plugin-svelte';
+import buble from 'rollup-plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
+
+var pkg = require('./package.json');
+
+export default {
+  entry: 'src/main.js',
+  dest: 'build/bundle-' + pkg.version + '.js',
+  format: 'iife',
+  plugins: [
+    resolve({
+      // use "module" field for ES6 module if possible
+      module: true, // Default: true
+
+      // use "jsnext:main" if possible
+      // – see https://github.com/rollup/rollup/wiki/jsnext:main
+      jsnext: true,  // Default: false
+
+      // use "main" field or index.js, even if it's not an ES6 module
+      // (needs to be converted from CommonJS to ES6
+      // – see https://github.com/rollup/rollup-plugin-commonjs
+      main: true,  // Default: true
+
+      // some package.json files have a `browser` field which
+      // specifies alternative files to load for people bundling
+      // for the browser. If that's you, use this option, otherwise
+      // pkg.browser will be ignored
+      browser: true,  // Default: false
+
+      // not all files you want to resolve are .js files
+      extensions: [ '.js', '.json' ],  // Default: ['.js']
+
+      // whether to prefer built-in modules (e.g. `fs`, `path`) or
+      // local ones with the same names
+      preferBuiltins: false,  // Default: true
+
+      // If true, inspect resolved files to check that they are
+      // ES2015 modules
+      modulesOnly: true // Default: false
+    }),
+    svelte({
+      // By default, all .html and .svelte files are compiled
+      extensions: [ '.html' ],
+
+      // You can restrict which files are compiled
+      // using `include` and `exclude`
+      /*include: [
+        'src/components/**.html'
+      ],*/
+
+      css: function ( css ) {
+        fs.writeFileSync( 'build/main-' + pkg.version + '.css', css );
+      }
+    }),
+    buble(),
+    serve({
+      // Folder to serve files from,
+      contentBase: '',
+
+      // Set to true to return index.html instead of 404
+      historyApiFallback: true,
+
+      // Options used in setting up server
+      host: 'localhost',
+      port: 3200
+    })
+  ]
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import App from './App.html';
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+let app = new App({
+  target: document.getElementById('app')
+});
